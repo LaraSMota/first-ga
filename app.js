9fb90306a3ab72929b801fd7dcf82c2bd9d9b648
MotaLara <lara.s.mota@gmail.com>
2018-11-06 22:33:27 -0300

    Função de fitness funcionando, função configurarSala implementada, geraIndividuo corrigido para obrigatoriamente ter destino e inicio no cromossomo

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+function configuraSala(numCol, numLin, inicio, destino){
+    let config = [numCol, numLin, inicio, destino];
+    return config;
+}
+
 function geraGene(maxI, minI, maxJ, minJ){
     let gene = new Array(2);
     //floor vs. round -> ver o que seria melhor com Otacilio;
@@ -7,93 +12,137 @@ function geraGene(maxI, minI, maxJ, minJ){
     return gene;
 }
 
-function geraIndividuo(){
+function geraIndividuo(config){
     let individuo = [];
+    const numCol = config[0];
+    const numLin = config[1];
+    const inicio = config[2];
+    const destino = config[3];
+    const tamGrid = numCol * numLin;
+    let verificaDestino; 
+    let verificaInicio;
 
-    //Crio o meu primeiro gene, a posição onde o robô começa;
-    //Proxima versão -> colocar para se de 0 a variaveis com numero de linhas e colunas da grid.
-    individuo.push(geraGene(2, 0, 2, 0));
+    do{
+        individuo.length = 0;
+        verificaDestino = false;
+        verificaInicio = false;
+        //Crio o meu primeiro gene, a posição onde o robô começa;
+        //Proxima versão -> colocar para se de 0 a variaveis com numero de linhas e colunas da grid.
+        individuo.push(geraGene((numCol - 1), 0, (numLin - 1), 0));
+        //Cria os genes seguintes levando o primeiro gene como ponto de partida
+        //Proxima versão -> colocar para ser de 1 ao tamanho total da grid
+        for(let count = 1; count < tamGrid; count++){
+            let Imin;
+            let Imax;
+            let Jmax;
+            let Jmin;
+           
+            //Seçõo para estabelecer os valores que podem gerar o proximo gene
+            //Como o robô pode se locomover apenas uma casa em qualquer direção, os valores podem ser uma unidade acima ou 
+            //      abaixo das coordenadas do gene anterior. Lembrando de que deve respeitar as bordas.
+            if(individuo[count-1][0] === 0){
+                Imin = Number(individuo[count-1][0]);
+            } else {
+                Imin = Number(individuo[count-1][0]) - 1;
+            } 
+            if(individuo[count-1][0] === (numCol - 1)){
+                Imax = Number(individuo[count-1][0]);
+            } else {
+                Imax = Number(individuo[count-1][0]) + 1;
+            }
+            if(individuo[count-1][1] === 0){
+                Jmin = Number(individuo[count-1][1]); 
+            } else {
+                Jmin = Number(individuo[count-1][1]) - 1;
+            }
+            if(individuo[count-1][1] === (numLin - 1)){
+                Jmax = Number(individuo[count-1][1]);
+            } else {
+                Jmax = Number(individuo[count-1][1]) + 1;
+            }
 
-    //Cria os genes seguintes levando o primeiro gene como ponto de partida
-    //Proxima versão -> colocar para ser de 1 ao tamanho total da grid
-    for(let count = 1; count < 9; count++){
-        let Imin;
-        let Imax;
-        let Jmax;
-        let Jmin;
-
-        //Seçõo para estabelecer os valores que podem gerar o proximo gene
-        //Como o robô pode se locomover apenas uma casa em qualquer direção, os valores podem ser uma unidade acima ou 
-        //      abaixo das coordenadas do gene anterior. Lembrando de que deve respeitar as bordas.
-        if(individuo[count-1][0] === 0){
-            Imin = Number(individuo[count-1][0]);
-        } else {
-            Imin = Number(individuo[count-1][0]) - 1;
-        } 
-        if(individuo[count-1][0] === 2){
-            Imax = Number(individuo[count-1][0]);
-        } else {
-            Imax = Number(individuo[count-1][0]) + 1;
+            //Salvando o novo gene criado ao final do array
+            individuo.push(geraGene(Imax, Imin, Jmax, Jmin));
         }
-        if(individuo[count-1][1] === 0){
-            Jmin = Number(individuo[count-1][1]); 
-        } else {
-            Jmin = Number(individuo[count-1][1]) - 1;
+        for(count = 0; count < individuo.length; count++){
+            if(individuo[count][0]===destino[0] && individuo[count][1]===destino[1]){
+                verificaDestino = true;
+            } 
+            if(individuo[count][0]===inicio[0] && individuo[count][1]===inicio[1]){
+                verificaInicio = true;
+            }
         }
-        if(individuo[count-1][1] === 2){
-            Jmax = Number(individuo[count-1][1]);
-        } else {
-            Jmax = Number(individuo[count-1][1]) + 1;
-        }
-
-        //Salvando o novo gene criado ao final do array
-        individuo.push(geraGene(Imax, Imin, Jmax, Jmin));
-    }
+    }while(verificaIndividuo(verificaDestino, verificaInicio) === false);
     return individuo;
 }
 
-function geraPopulacao(popTamanho){
+function verificaIndividuo(verificaDestino, verificaInicio){
+    if(verificaDestino && verificaInicio){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function geraPopulacao(config, popTamanho){
     let populacao = [];
 
     for(let count = 0; count < popTamanho; count++){
-        populacao.push(geraIndividuo());
+        populacao.push(geraIndividuo(config));
     }
     return populacao;
 }
 
-function calculaFitness(inicio, destino, populacao){
+function calculaFitness(config, populacao){
     let popFitness = [];
+    const inicio = config[2];
+    const destino = config[3];
     
     for(let count = 0; count <populacao.length; count++){
         let individuo = populacao[count];
+        let pontoFinal;
+        let pontoInicial;
+        let distancia;
+        let fitness;
 
-        //DEVERIA RETORNAR A POSIÇÃO NO ARRAY ONDE O INICIO E O FIM APARECEM
-        let pontoInicial = (individuo).indexOf(inicio);//NÃO TÁ FUNCIONANDO
-        let pontoFinal = (individuo).indexOf(destino);//NÃO TÁ FUNCIONANDO
-
-        if(pontoInicial === (-1) || pontoFinal === (-1)){//indexOf retorna -1 quanndo o elemento não existe
-            popFitness[count] = -100;
-        } else {
-            //Calcula o fitness -> distancia entre o ponto inicial e o destino
-            popFitness[count] = 100/(pontoFinal - pontoInicial);//podia ser 1 ao inves de 100, mas os numeros ficariam muito proximos e quebrados
-
-            //verifica se o destino veio antes do inicio e corrige
-            if(popFitness[count] < 0){
-                populacao[count].reverse();//inverte o individuo para corrigir
-                popFitness[count] = popFitness[count] * (-1);//deixa o fitness positivo
+        for(let a = 0; a < individuo.length; a++){
+            if(individuo[a][0]===destino[0] && individuo[a][1]===destino[1]){
+                pontoFinal = a;
             }
+            if(individuo[a][0]===inicio[0] && individuo[a][1]===inicio[1]){
+                pontoInicial = a;
+            }
+        }
+        //Calcula o fitness -> distancia entre o ponto inicial e o destino
+        distancia = pontoFinal - pontoInicial;
+        fitness = 100/distancia;//podia ser 1 ao inves de 100, mas os numeros ficariam muito proximos e quebrados
+        popFitness.push([fitness, count]); 
+
+        //verifica se o destino veio antes do inicio e corrige
+        if(popFitness[count][0] < 0){
+            populacao[count].reverse();//inverte o individuo para corrigir
+            popFitness[count][0] = popFitness[count][0] * (-1);//deixa o fitness positivo
         }
     }
     //Implementar o "reorganizar array" para ficar em ordem crescente de fitness
     return popFitness;
 }
+//function organizaPopulacao(populacao, popFitness){
+//    let transicao = populacao;
+//    for(let count = 0; count < populacao.length; count++){
+//        let ind = popFitness[count][1];
+        
+//    }
+//}
 //Seleção pelo metodo da roleta
 function selecao(populacao, popFitness){
     let somaFitness = 0;
     let selecionados = [];
 
+    //organizaPopulacao(populacao, popFitness);
+
     for(let count = 0; count < popFitness.length; count++){
-        somaFitness += popFitness[count];
+        somaFitness += popFitness[count][0];
     }
 
     for (count = 0; count < popFitness.length; count++){
@@ -103,18 +152,14 @@ function selecao(populacao, popFitness){
 
         while(aux < posicaoSelecionado){
             selecionados[count] = populacao[posicao];
-            aux += popFitness[posicao];
+            aux += popFitness[posicao][0];
             posicao++;
         }
     }
     return selecionados;    
 }
-
-//let inicio = [0,0];
-//let destino = [2,2];
-let populacao = geraPopulacao(10);
-let popFitness = [1,1,1,2,3,3,4,4,5,10];//Para Testar se a seleção está funcionando
-console.log(selecao(populacao, popFitness));
-
-
+let config = configuraSala(5, 5, [0,0], [4,4]);
+let populacao = geraPopulacao(config, 10);
+let fitness = calculaFitness(config, populacao);
+console.log(fitness);
 
